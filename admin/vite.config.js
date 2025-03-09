80df08e1db7c6d318c715b3adf92f50e86a35264
ShubhamShinde06 <officialshubhamorg@gmail.com>
2025-03-09 12:05:55 +0530

    updated

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':"https://cloth-e-commerce-47l9.onrender.com"
-    }
-  },
-  plugins: [react()],
-  server: {port:  5174}
+  plugins: [react()]
 })
